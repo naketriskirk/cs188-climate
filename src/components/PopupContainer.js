c293ba16c6e25410da5199ec0468df063cd79253
William Wong <wwong10th@gmail.com>
2025-05-27 08:47:11 -0700

    Update API

--- src/components/PopupContainer.js
+++ src/components/PopupContainer.js
@@ -3,32 +3,28 @@ import './PopupContainer.css';
 import RatingCircle from '../RatingCircle';
 
 
-const PopupContainer = ({company_name, avg_rating, transparency, environment, social, governance}) => {
+const PopupContainer = ({data}) => {
 
     return (
         <div className="containerStyle"> 
-            <h1>{company_name}</h1>
+            <h1>{data.name}</h1>
 
             <div class="overall-score-container">
                 <h2>Overall Score:</h2>
-                <RatingCircle value={avg_rating} />
+                <RatingCircle value={data.overallRating.score_out_of_100} />
             </div>
             <div style={{ display: 'flex', gap: '16px', marginTop: '16px' }}>
                 <div class="score-container">
-                    <h2>Transparency</h2>
-                    <RatingCircle value={transparency} />
-                </div>
-                <div class="score-container">
                     <h2>Environment</h2>
-                    <RatingCircle value={environment} />
+                    <RatingCircle value={data.environment.score_out_of_100} />
                 </div>
                 <div class="score-container">
-                    <h2>Social</h2>
-                    <RatingCircle value={social} />
+                    <h2>Labour</h2>
+                    <RatingCircle value={data.labour.score_out_of_100} />
                 </div>
                 <div class="score-container">
-                    <h2>Governance</h2>
-                    <RatingCircle value={governance} />
+                    <h2>Animal</h2>
+                    <RatingCircle value={data.animal.score_out_of_100} />
                 </div>
             </div>
         </div>
