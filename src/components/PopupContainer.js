1d784c008f9f0e7f3b54e69a88572f4f8641aa1b
SanjnaT41756 <sanjnatailor@g.ucla.edu>
2025-05-27 09:10:20 -0700

    create styling classes

--- src/components/PopupContainer.js
+++ src/components/PopupContainer.js
@@ -7,28 +7,24 @@ const PopupContainer = ({company_name, avg_rating, transparency, environment, so
 
     return (
         <div className="containerStyle"> 
-            <h1>{company_name}</h1>
 
             <div class="overall-score-container">
-                <h2>Overall Score:</h2>
-                <RatingCircle value={avg_rating} />
+                <h1>{data.name}</h1>
+                <h2 className='header'>Overall Score:</h2>
+                <RatingCircle value={data.overallRating.score_out_of_100}/>
             </div>
-            <div style={{ display: 'flex', gap: '16px', marginTop: '16px' }}>
+            <div className="subscore-container">
                 <div class="score-container">
-                    <h2>Transparency</h2>
-                    <RatingCircle value={transparency} />
+                    <h2 className='subheader'>Environment</h2>
+                    <RatingCircle value={data.environment.score_out_of_100} />
                 </div>
                 <div class="score-container">
-                    <h2>Environment</h2>
-                    <RatingCircle value={environment} />
+                    <h2 className='subheader'>Labour</h2>
+                    <RatingCircle value={data.labour.score_out_of_100} />
                 </div>
                 <div class="score-container">
-                    <h2>Social</h2>
-                    <RatingCircle value={social} />
-                </div>
-                <div class="score-container">
-                    <h2>Governance</h2>
-                    <RatingCircle value={governance} />
+                    <h2 className='subheader'>Animal</h2>
+                    <RatingCircle value={data.animal.score_out_of_100} />
                 </div>
             </div>
         </div>
